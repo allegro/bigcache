54410bf9758d97d00c10cc1cc3df7834678ee6c9
Lukasz Druminski <lukasz.druminski@allegrogroup.com>
2016-03-30 11:12:00 +0200

    Issue #4 | Implementing hard max cache size

--- bigcache_test.go
+++ bigcache_test.go
@@ -27,7 +27,7 @@ func TestConstructCacheWithDefaultHasher(t *testing.T) {
 	t.Parallel()
 
 	// given
-	cache, _ := NewBigCache(Config{16, 5 * time.Second, 10, 256, false, nil})
+	cache, _ := NewBigCache(Config{16, 5 * time.Second, 10, 256, false, nil, 0})
 
 	assert.IsType(t, fnv64a{}, cache.hash)
 }
@@ -36,7 +36,7 @@ func TestWillReturnErrorOnInvalidNumberOfPartitions(t *testing.T) {
 	t.Parallel()
 
 	// given
-	cache, error := NewBigCache(Config{18, 5 * time.Second, 10, 256, false, nil})
+	cache, error := NewBigCache(Config{18, 5 * time.Second, 10, 256, false, nil, 0})
 
 	assert.Nil(t, cache)
 	assert.Error(t, error, "Shards number must be power of two")
@@ -46,7 +46,7 @@ func TestEntryNotFound(t *testing.T) {
 	t.Parallel()
 
 	// given
-	cache, _ := NewBigCache(Config{16, 5 * time.Second, 10, 256, false, nil})
+	cache, _ := NewBigCache(Config{16, 5 * time.Second, 10, 256, false, nil, 0})
 
 	// when
 	_, err := cache.Get("nonExistingKey")
@@ -60,7 +60,7 @@ func TestTimingEviction(t *testing.T) {
 
 	// given
 	clock := mockedClock{value: 0}
-	cache, _ := newBigCache(Config{1, time.Second, 1, 256, false, nil}, &clock)
+	cache, _ := newBigCache(Config{1, time.Second, 1, 256, false, nil, 0}, &clock)
 
 	// when
 	cache.Set("key", []byte("value"))
@@ -77,7 +77,7 @@ func TestEntryUpdate(t *testing.T) {
 
 	// given
 	clock := mockedClock{value: 0}
-	cache, _ := newBigCache(Config{1, 6 * time.Second, 1, 256, false, nil}, &clock)
+	cache, _ := newBigCache(Config{1, 6 * time.Second, 1, 256, false, nil, 0}, &clock)
 
 	// when
 	cache.Set("key", []byte("value"))
@@ -91,11 +91,45 @@ func TestEntryUpdate(t *testing.T) {
 	assert.Equal(t, []byte("value2"), cachedValue)
 }
 
+func TestOldestEntryDeletionWhenMaxCacheSizeIsReached(t *testing.T) {
+	t.Parallel()
+
+	// given
+	cache, _ := NewBigCache(Config{1, 5 * time.Second, 1, 1, false, nil, 1})
+
+	// when
+	cache.Set("key1", blob('a', 1024*400))
+	cache.Set("key2", blob('b', 1024*400))
+	cache.Set("key3", blob('c', 1024*800))
+
+	_, key1Err := cache.Get("key1")
+	_, key2Err := cache.Get("key2")
+	entry3, _ := cache.Get("key3")
+
+	// then
+	assert.EqualError(t, key1Err, "Entry \"key1\" not found")
+	assert.EqualError(t, key2Err, "Entry \"key2\" not found")
+	assert.Equal(t, blob('c', 1024*800), entry3)
+}
+
+func TestEntryBiggerThanMaxShardSizeError(t *testing.T) {
+	t.Parallel()
+
+	// given
+	cache, _ := NewBigCache(Config{1, 5 * time.Second, 1, 1, false, nil, 1})
+
+	// when
+	err := cache.Set("key1", blob('a', 1024*1025))
+
+	// then
+	assert.EqualError(t, err, "Entry is bigger than max shard size.")
+}
+
 func TestHashCollision(t *testing.T) {
 	t.Parallel()
 
 	// given
-	cache, _ := NewBigCache(Config{16, 5 * time.Second, 10, 256, true, hashStub(5)})
+	cache, _ := NewBigCache(Config{16, 5 * time.Second, 10, 256, true, hashStub(5), 0})
 
 	// when
 	cache.Set("liquid", []byte("value"))
@@ -131,4 +165,12 @@ func (mc *mockedClock) epoch() int64 {
 
 func (mc *mockedClock) set(value int64) {
 	mc.value = value
+}
+
+func blob(char byte, len int) []byte {
+	b := make([]byte, len)
+	for index := range b {
+		b[index] = char
+	}
+	return b
 }
--- config.go
+++ config.go
@@ -4,20 +4,24 @@ import "time"
 
 // Config for BigCache
 type Config struct {
-	// Number of cache shards
-	// Proper value must be a power of two
+	// Number of cache shards, value must be a power of two
 	Shards int
 	// Time after which entry can be evicted
 	LifeWindow time.Duration
-	// Max number of entries in life window. Used to allocate proper size of cache in every shard.
-	// When proper value is set then cache will not allocate additional memory
+	// Max number of entries in life window. Used only to calculate initial size for cache shards.
+	// When proper value is set then additional memory allocation does not occur.
 	MaxEntriesInWindow int
-	// Max size of entry in bytes. Used to allocate proper size of cache in every shard.
+	// Max size of entry in bytes. Used only to calculate initial size for cache shards.
 	MaxEntrySize int
 	// Verbose mode prints information about new memory allocation
 	Verbose bool
 	// Hasher used to map between string keys and unsigned 64bit integers, by default fnv64 hashing is used.
 	Hasher Hasher
+	// HardMaxCacheSize is a limit for cache size in MB. Cache will not allocate more memory than this limit.
+	// It can protect application from consuming all available memory on machine, therefore from running OOM Killer.
+	// Default value is 0 which means unlimited size. When the limit is higher than 0 and reached then
+	// the oldest entries are overridden for the new ones.
+	HardMaxCacheSize int
 }
 
 // DefaultConfig initializes config with default values.
@@ -30,5 +34,6 @@ func DefaultConfig(eviction time.Duration) Config {
 		MaxEntrySize:       500,
 		Verbose:            true,
 		Hasher:             newDefaultHasher(),
+		HardMaxCacheSize:   0,
 	}
 }
