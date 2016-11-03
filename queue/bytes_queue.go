a7f875908b34ca517e1195c3772fffcf287be18f
Mateusz Gajewski <mateusz.gajewski@allegrogroup.com>
2016-11-03 11:13:23 +0100

    Add one more test

--- queue/bytes_queue.go
+++ queue/bytes_queue.go
@@ -59,6 +59,7 @@ func (q *BytesQueue) Reset() {
 	q.head = leftMarginIndex
 	q.rightMargin = leftMarginIndex
 	q.capacity = q.initialCapacity
+	q.count = 0
 }
 
 // Push copies entry at the end of queue and moves tail pointer. Allocates more space if needed.
