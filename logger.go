394820e312b806415be12b8494e6b840b2e50992
lxfontes <lxfontes@gmail.com>
2017-10-09 11:22:36 -0400

    logger: 'var _ = x' statement comment

--- logger.go
+++ logger.go
@@ -10,6 +10,9 @@ type Logger interface {
 	Printf(format string, v ...interface{})
 }
 
+// this is a safeguard, breaking on compile time in case
+// `log.Logger` does not adhere to our `Logger` interface.
+// see https://golang.org/doc/faq#guarantee_satisfies_interface
 var _ Logger = &log.Logger{}
 
 // DefaultLogger returns a `Logger` implementation
