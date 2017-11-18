39a9416e6936d33fcee041c0d3a427787f009e27
Mike Lloyd <mike@reboot3times.org>
2017-11-17 21:52:21 -0500

    BigCache HTTP Server complete!
    Quantification of a basic HTTP server for BigCache! This finalises
    the basic HTTP server implementation I set forth to create.
    
    Adds:
    DELETE to the cache API.
    Stats API.
    API tests.
    Version command-line flag.
    
    Modifies:
    Moved handlers and middleware to their own files.
    README for more details.
    
    Signed-off-by: Mike Lloyd <mike@reboot3times.org>

--- stats.go
+++ stats.go
@@ -3,13 +3,13 @@ package bigcache
 // Stats stores cache statistics
 type Stats struct {
 	// Hits is a number of successfully found keys
-	Hits int64
+	Hits int64 `json:"hits"`
 	// Misses is a number of not found keys
-	Misses int64
+	Misses int64 `json:"misses"`
 	// DelHits is a number of successfully deleted keys
-	DelHits int64
+	DelHits int64 `json:"delete_hits"`
 	// DelMisses is a number of not deleted keys
-	DelMisses int64
+	DelMisses int64 `json:"delete_misses"`
 	// Collisions is a number of happened key-collisions
-	Collisions int64
+	Collisions int64 `json:"collisions"`
 }
