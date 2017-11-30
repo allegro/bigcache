62db1442811378ae21b10e241fa226bfdc326aca
Mike Lloyd <mike@reboot3times.org>
2017-11-30 04:42:23 -0700

    HTTP Server is done! (#60)
    Quantification of a basic HTTP server for BigCache! This finalises
    the basic HTTP server implementation I set forth to create.
    
    Adds:
    Basic middleware implementation.
    DELETE to the cache API.
    Stats API.
    API tests.
    Version command-line flag.
    
    Modifies:
    Moved handlers and middleware to their own files.
    README for more details.

--- server/README.md
+++ server/README.md
@@ -1,25 +1,38 @@
 # BigCache HTTP Server
 
-This is a basic HTTP server implementation for BigCache. It has a basic RESTful API.
+This is a basic HTTP server implementation for BigCache. It has a basic RESTful API and is designed for easy operational deployments. This server is intended to be consumed as a standalone executable, for things like Cloud Foundry, Heroku, etc. A design goal is versatility, so if you want to cache pictures, software artifacts, text, or any type of bit, the BigCache HTTP Server should fit your needs.
 
 ```bash
-GET /api/v1/cache/{key}
-PUT /api/v1/cache/{key}
+# cache API.
+GET         /api/v1/cache/{key}
+PUT         /api/v1/cache/{key}
+DELETE      /api/v1/cache/{key}
+
+# stats API.
+GET         /api/v1/stats
 ```
 
-This server is designed to be consumed as a standalone executable, for things like Cloud Foundry, Heroku, etc.
+The cache API is designed for ease-of-use caching and accepts any content type. The stats API will return hit and miss statistics about the cache since the last time the server was started - they will reset whenever the server is restarted.
+
+### Notes for Operators
+
+1. No SSL support, currently.
+1. No authentication, currently.
+1. Stats from the stats API are not persistent.
+1. The easiest way to clean the cache is to restart the process; it takes less than a second to initialise.
+1. There is no replication or clustering.
 
 ### Command-line Interface
 
 ```powershell
 PS C:\go\src\github.com\mxplusb\bigcache\server> .\server.exe -h
 Usage of C:\go\src\github.com\mxplusb\bigcache\server\server.exe:
-  -lifetime int
-        Lifetime, in minutes, of each cache object. (default 10)
+  -lifetime duration
+        Lifetime of each cache object. (default 10m0s)
   -logfile string
         Location of the logfile.
   -max int
-        Maximum amount of objects in the cache. (default 8192)
+        Maximum amount of data in the cache in MB. (default 8192)
   -maxInWindow int
         Used only in initial memory allocation. (default 600000)
   -maxShardEntrySize int
@@ -29,7 +42,8 @@ Usage of C:\go\src\github.com\mxplusb\bigcache\server\server.exe:
   -shards int
         Number of shards for the cache. (default 1024)
   -v    Verbose logging.
-
+  -version
+        Print server version.
 ```
 
 Example:
@@ -82,7 +96,7 @@ $ ./server
 2017/11/16 22:50:23 request took 9000ns.
 ```
 
-### Acquiring
+### Acquiring Natively
 
 This is native Go with no external dependencies, so it will compile for all supported Golang platforms. To build:
 
--- server/server.go
+++ server/server.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"flag"
-	"io/ioutil"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
-	"time"
 
 	"github.com/allegro/bigcache"
 )
@@ -20,11 +18,16 @@ const (
 
 	// path to cache.
 	cachePath = apiBasePath + "cache/"
+	statsPath = apiBasePath + "stats"
+
+	// server version.
+	version = "1.0.0"
 )
 
 var (
 	port    int
 	logfile string
+	ver     bool
 
 	// cache-specific settings.
 	cache  *bigcache.BigCache
@@ -40,10 +43,16 @@ func init() {
 	flag.IntVar(&config.MaxEntrySize, "maxShardEntrySize", 500, "The maximum size of each object stored in a shard. Used only in initial memory allocation.")
 	flag.IntVar(&port, "port", 9090, "The port to listen on.")
 	flag.StringVar(&logfile, "logfile", "", "Location of the logfile.")
+	flag.BoolVar(&ver, "version", false, "Print server version.")
 }
 
 func main() {
 	flag.Parse()
+
+	if ver {
+		fmt.Printf("BigCache HTTP Server v%s", version)
+		os.Exit(0)
+	}
 
 	var logger *log.Logger
 
@@ -67,91 +76,10 @@ func main() {
 
 	// let the middleware log.
 	http.Handle(cachePath, serviceLoader(cacheIndexHandler(), requestMetrics(logger)))
+	http.Handle(statsPath, serviceLoader(statsIndexHandler(), requestMetrics(logger)))
 
 	logger.Printf("starting server on :%d", port)
 
 	strPort := ":" + strconv.Itoa(port)
 	log.Fatal("ListenAndServe: ", http.ListenAndServe(strPort, nil))
-}
-
-// our base middleware implementation.
-type service func(http.Handler) http.Handler
-
-// chain load middleware services.
-func serviceLoader(h http.Handler, svcs ...service) http.Handler {
-	for _, svc := range svcs {
-		h = svc(h)
-	}
-	return h
-}
-
-func cacheIndexHandler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			getCacheHandler(w, r)
-		case http.MethodPut:
-			putCacheHandler(w, r)
-		}
-	})
-}
-
-// middleware for request length metrics.
-func requestMetrics(l *log.Logger) service {
-	return func(h http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			start := time.Now()
-			h.ServeHTTP(w, r)
-			l.Printf("request took %vns.", time.Now().Sub(start).Nanoseconds())
-		})
-	}
-}
-
-// handles get requests.
-func getCacheHandler(w http.ResponseWriter, r *http.Request) {
-	target := r.URL.Path[len(cachePath):]
-	if target == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("can't get a key if there is no key."))
-		log.Print("empty request.")
-		return
-	}
-	entry, err := cache.Get(target)
-	if err != nil {
-		errMsg := (err).Error()
-		if strings.Contains(errMsg, "not found") {
-			log.Print(err)
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-		log.Print(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.Write(entry)
-}
-
-func putCacheHandler(w http.ResponseWriter, r *http.Request) {
-	target := r.URL.Path[len(cachePath):]
-	if target == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("can't put a key if there is no key."))
-		log.Print("empty request.")
-		return
-	}
-
-	entry, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Print(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if err := cache.Set(target, []byte(entry)); err != nil {
-		log.Print(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	log.Printf("stored \"%s\" in cache.", target)
-	w.WriteHeader(http.StatusCreated)
 }
