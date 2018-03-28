08536f227fa9bc9ae3c9375086382fd24ed8bcba
Mike Lloyd <mike@reboot3times.org>
2018-03-27 19:32:39 -0600

    mostly finished middleware tests.
    this should cover the request length of time middleware and increase
    some of the code coverage.
    
    merging from master as well.
    
    Signed-off-by: Mike Lloyd <mike@reboot3times.org>

--- server/middleware_test.go
+++ server/middleware_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -25,4 +27,21 @@ func TestServiceLoader(t *testing.T) {
 	if status := rr.Code; status != http.StatusAccepted {
 		t.Errorf("handlers not loading properly. want: 202, got: %d", rr.Code)
 	}
+}
+
+func TestRequestMetrics(t *testing.T) {
+	var b bytes.Buffer
+	logger := log.New(&b, "", log.LstdFlags)
+	req, err := http.NewRequest("GET", "/api/v1/cache/empty", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	rr := httptest.NewRecorder()
+	testHandlers := serviceLoader(cacheIndexHandler(), requestMetrics(logger))
+	testHandlers.ServeHTTP(rr, req)
+	targetTestString := b.String()
+	if len(targetTestString) == 0 {
+		t.Errorf("we are not logging request length strings.")
+	}
+	t.Log(targetTestString)
 }
