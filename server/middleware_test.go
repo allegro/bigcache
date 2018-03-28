d10c27f0072aae8d7064eba20d33318cd90c9a86
Mike Lloyd <mike@reboot3times.org>
2018-03-27 19:09:13 -0600

    updated middleware test.
    it now works with httptest and covers more of the functionality.
    
    Signed-off-by: Mike Lloyd <mike@reboot3times.org>

--- server/middleware_test.go
+++ server/middleware_test.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func emptyTestHandler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		return
-	})
+func emptyTestHandler() service {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusAccepted)
+		})
+	}
 }
 
 func TestServiceLoader(t *testing.T) {
-	_ = serviceLoader(emptyTestHandler())
+	req, err := http.NewRequest("GET", "/api/v1/stats", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	rr := httptest.NewRecorder()
+	testHandlers := serviceLoader(cacheIndexHandler(), emptyTestHandler())
+	testHandlers.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusAccepted {
+		t.Errorf("handlers not loading properly. want: 202, got: %d", rr.Code)
+	}
 }
