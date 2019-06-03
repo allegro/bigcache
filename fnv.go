e7be5e7f081d2b022b09b52e8dcc13393e497cb2
hiqinternational <51332507+hiqinternational@users.noreply.github.com>
2019-06-04 00:18:50 +0800

    Update fnv.go

--- fnv.go
+++ fnv.go
@@ -12,15 +12,16 @@ func newDefaultHasher() Hasher {
 }
 
 type fnv64a struct{}
-
+/*
 const (
 	// offset64 FNVa offset basis. See https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function#FNV-1a_hash
 	offset64 = 14695981039346656037
 	// prime64 FNVa prime value. See https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function#FNV-1a_hash
 	prime64 = 1099511628211
 )
+*/
 // Sum64 gets the string and returns its uint64 hash value.
-func (f fnv64a) Sum64(key string) uint64 {
+func (f fnv64a) Sum64(key []byte) uint64 {
 //	var hash uint64 = offset64
 	hash := xxhash.Sum64([]byte(key))
 //	for i := 0; i < len(key); i++ {
