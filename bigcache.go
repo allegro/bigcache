485451134eec569748eeeec9146bfc9599dbd86b
Jay Gheewala <jgheewala@signalfx.com>
2022-01-18 05:54:35 -0500

    Expired reason iota fix (#255)
    * Go defaults to "0"" so in case we want to return
    EntryStatus back to the caller "Expired" cannot be differentiated.
    Fixing this by default "_" to 0 and incremental RemoveReasons
    
    comment was missing for GetWithInfo api so updated that
    
    test: ran all unit test cases
    
    * Entry RemoveReason is iota based with default value being 0
    
    Currently all const for RemovedReason are explicitly
    set to avoid any breaking changes. For v3 migrating the reasons
    to iota makes code readability and managing addition of more reasons
    easier in the future.
    
    Test: Validated all test cases run successfully.
    
    Co-authored-by: Mike Lloyd <mike@reboot3times.org>
    Co-authored-by: jgheewala <jgheewala@splunk.com>

--- bigcache.go
+++ bigcache.go
@@ -32,13 +32,14 @@ type Response struct {
 type RemoveReason uint32
 
 const (
+	_ RemoveReason = iota
 	// Expired means the key is past its LifeWindow.
-	Expired = RemoveReason(1)
+	Expired
 	// NoSpace means the key is the oldest and the cache size was at its maximum when Set was called, or the
 	// entry exceeded the maximum shard size.
-	NoSpace = RemoveReason(2)
+	NoSpace
 	// Deleted means Delete was called and this key was removed as a result.
-	Deleted = RemoveReason(3)
+	Deleted
 )
 
 // NewBigCache initialize new instance of BigCache
