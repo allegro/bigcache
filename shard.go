8f80589dbe49e3c6283bf916efd8f2b7115af4cd
zhangyaping-CN <110162184+zhangyaping-CN@users.noreply.github.com>
2022-08-02 21:07:51 +0800

    No need to del elem when config.CleanWindow>0 at method:Set (#325)
    Co-authored-by: Yaping Zhang <yaping.zhang@shopee.com>

--- shard.go
+++ shard.go
@@ -30,6 +30,7 @@ type cacheShard struct {
 
 	hashmapStats map[uint64]uint32
 	stats        Stats
+	cleanEnabled bool
 }
 
 func (s *cacheShard) getWithInfo(key string, hashedKey uint64) (entry []byte, resp Response, err error) {
@@ -129,8 +130,10 @@ func (s *cacheShard) set(key string, hashedKey uint64, entry []byte) error {
 		}
 	}
 
-	if oldestEntry, err := s.entries.Peek(); err == nil {
-		s.onEvict(oldestEntry, currentTimestamp, s.removeOldestEntry)
+	if !s.cleanEnabled {
+		if oldestEntry, err := s.entries.Peek(); err == nil {
+			s.onEvict(oldestEntry, currentTimestamp, s.removeOldestEntry)
+		}
 	}
 
 	w := wrapEntry(currentTimestamp, hashedKey, key, entry, &s.entryBuffer)
@@ -151,8 +154,10 @@ func (s *cacheShard) set(key string, hashedKey uint64, entry []byte) error {
 func (s *cacheShard) addNewWithoutLock(key string, hashedKey uint64, entry []byte) error {
 	currentTimestamp := uint64(s.clock.Epoch())
 
-	if oldestEntry, err := s.entries.Peek(); err == nil {
-		s.onEvict(oldestEntry, currentTimestamp, s.removeOldestEntry)
+	if !s.cleanEnabled {
+		if oldestEntry, err := s.entries.Peek(); err == nil {
+			s.onEvict(oldestEntry, currentTimestamp, s.removeOldestEntry)
+		}
 	}
 
 	w := wrapEntry(currentTimestamp, hashedKey, key, entry, &s.entryBuffer)
@@ -175,8 +180,10 @@ func (s *cacheShard) setWrappedEntryWithoutLock(currentTimestamp uint64, w []byt
 		}
 	}
 
-	if oldestEntry, err := s.entries.Peek(); err == nil {
-		s.onEvict(oldestEntry, currentTimestamp, s.removeOldestEntry)
+	if !s.cleanEnabled {
+		if oldestEntry, err := s.entries.Peek(); err == nil {
+			s.onEvict(oldestEntry, currentTimestamp, s.removeOldestEntry)
+		}
 	}
 
 	for {
@@ -436,5 +443,6 @@ func initNewShard(config Config, callback onRemoveCallback, clock clock) *cacheS
 		clock:        clock,
 		lifeWindow:   uint64(config.LifeWindow.Seconds()),
 		statsEnabled: config.StatsEnabled,
+		cleanEnabled: config.CleanWindow > 0,
 	}
 }
