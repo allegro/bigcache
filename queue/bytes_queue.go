8614d4ad5fe2679b17f3a6327d7f74b2402755d7
huangruofei <huangruofei@bytedance.com>
2024-07-03 18:00:43 +0800

    feat: enable mmap to alloc on unix-like system

--- queue/bytes_queue.go
+++ queue/bytes_queue.go
@@ -57,22 +57,6 @@ func getNeededSize(length int) int {
 	return length + header
 }
 
-// NewBytesQueue initialize new bytes queue.
-// capacity is used in bytes array allocation
-// When verbose flag is set then information about memory allocation are printed
-func NewBytesQueue(capacity int, maxCapacity int, verbose bool) *BytesQueue {
-	return &BytesQueue{
-		array:        make([]byte, capacity),
-		capacity:     capacity,
-		maxCapacity:  maxCapacity,
-		headerBuffer: make([]byte, binary.MaxVarintLen32),
-		tail:         leftMarginIndex,
-		head:         leftMarginIndex,
-		rightMargin:  leftMarginIndex,
-		verbose:      verbose,
-	}
-}
-
 // Reset removes all entries from queue
 func (q *BytesQueue) Reset() {
 	// Just reset indexes
